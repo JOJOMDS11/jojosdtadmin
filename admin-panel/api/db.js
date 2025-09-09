eeead2cfa8ac44c843bab6c655bfe4a1d3a316f0
JOJOMDS11 <padrejojos@gmail.com>
2025-09-09 19:51:41 -0300

    Update db.js

--- admin-panel/api/db.js
+++ admin-panel/api/db.js
@@ -28,11 +28,7 @@ async function createConnection() {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            port: process.env.DB_PORT || 3306,
-            ssl: {
-                rejectUnauthorized: false
-            },
-            connectTimeout: 30000
+            port: process.env.DB_PORT
         });
 
         console.log('✅ Conectado ao banco de dados MySQL com sucesso!');
